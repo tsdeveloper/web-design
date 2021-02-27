3e74b9af83194b01a7be5eb2686505cb56907ab0
Thiago <ts.analista@gmail.com>
2021-02-27 00:40:34 -0300

    fix: learn display element block, inline, inline-block

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formatting</title>
+    <link rel="stylesheet" type="text/css" href="./dist/css/main.css">
+</head>
+
+<body>
+    <main class="main-page">
+        <section class="content-main">
+            <p>Lorem, ipsum <span class="invert inline-block bigger">Inline Block</span> dolor sit amet <span class="invert inline bigger">Inline</span> adipisicing elit. Quibusdam, rerum! Harum maiores recusandae
+            <span class="invert block bigger">Block</span> accusamus exercitationem accusantium, id quibusdam soluta optio voluptas error iste architecto nihil
+                ipsum sapiente cum, autem minus!
+                Quibusdam, quidem officiis, qui consequatur eveniet hic voluptatibus, pariatur itaque placeat aliquid
+                culpa repudiandae. Exercitationem temporibus placeat itaque, obcaecati cum quasi optio, id sapiente
+                inventore dicta necessitatibus eum nihil consectetur.
+                Velit at autem temporibus. Sed voluptate laborum earum fugit ipsam eos minus labore a esse fuga placeat
+                quae tempora excepturi, hic, ex voluptatem officia dolores nobis optio. Excepturi, amet animi!
+                Dolorum hic totam, vero soluta corporis voluptatem quibusdam eaque possimus, tenetur ratione quisquam
+                cupiditate esse enim! Officia beatae minima dolores quia explicabo, culpa asperiores deleniti itaque?
+                Vitae facilis exercitationem error.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, qui suscipit modi nemo cumque, ipsa
+                eaque dolor sequi incidunt quidem delectus voluptates non quasi debitis sed illo. Omnis, distinctio
+                deleniti!
+                Debitis iste nemo officiis quaerat fuga a iure voluptatibus corrupti cupiditate perferendis consequuntur
+                doloremque, id odit neque alias illo quibusdam doloribus quam adipisci. Quae ducimus suscipit, mollitia
+                beatae necessitatibus quas!
+                Dolor quae, hic enim recusandae ad iusto esse explicabo laboriosam dolorum ratione pariatur porro
+                distinctio numquam cupiditate voluptatem voluptate deleniti blanditiis nemo, architecto illo libero
+                consequatur? Dolor provident eum natus.
+                Obcaecati, culpa. Unde debitis porro assumenda dolor non molestiae est, excepturi totam voluptatibus aut
+                repellendus adipisci, cum culpa id fugiat, at molestias quidem minus quam earum quisquam deserunt harum
+                iure!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem temporibus quasi enim quisquam? Illo
+                similique nihil est quis dignissimos quae ea eos pariatur. Enim fuga nobis necessitatibus temporibus,
+                dolore dolorem.
+                Quia vero eligendi, rem ipsam iste aliquid fugit expedita totam, suscipit ratione eos dolores
+                perferendis nihil, iusto commodi id distinctio perspiciatis debitis tempora! Sunt animi nisi voluptatem
+                earum laboriosam. Harum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt totam quas inventore cupiditate! Esse
+                atque voluptas dolorum! Iste hic soluta quos pariatur sit, et voluptate impedit, exercitationem eveniet
+                ad fugit.
+                Molestias omnis iusto quos sapiente perspiciatis sequi tempore cupiditate voluptatibus vel nostrum
+                itaque aliquam, esse adipisci dicta at porro saepe. Modi rerum fugit consequuntur porro quas eaque ea
+                earum corporis!
+                Molestias harum id, incidunt modi deserunt corrupti eligendi voluptatibus enim vero ex, dolorem
+                obcaecati molestiae ratione doloribus cupiditate quis consequuntur repellendus delectus mollitia ipsum
+                nulla animi dolores fugit sint. Enim.
+                Earum facilis temporibus odit repudiandae ullam id consequuntur voluptatem deserunt, adipisci, nemo
+                architecto eum necessitatibus ratione quae. Dolores veniam, eveniet quod incidunt quia, voluptatum
+                voluptas quasi, minima ex porro corrupti?</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse facilis asperiores impedit dicta
+                distinctio delectus eius, perferendis quasi reprehenderit laboriosam inventore, aut facere sed aliquam
+                sequi amet blanditiis eligendi velit.</p>
+        </section>
+    </main>
+</body>
+
+</html>
