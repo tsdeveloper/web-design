6b656e01a4aa8a3ddca891a7aed85120cd2ed7f1
Thiago <ts.analista@gmail.com>
2021-02-27 03:17:48 -0300

    fix: learn main menu center

--- index.php
+++ index.php
@@ -11,48 +11,20 @@
 
 <body>
     <main class="main-page">
-        <section class="content-main">
-            <p>Lorem, ipsum <span class="invert inline-block bigger">Inline Block</span> dolor sit amet <span class="invert inline bigger">Inline</span> adipisicing elit. Quibusdam, rerum! Harum maiores recusandae
-            <span class="invert block bigger">Block</span> accusamus exercitationem accusantium, id quibusdam soluta optio voluptas error iste architecto nihil
-                ipsum sapiente cum, autem minus!
-                Quibusdam, quidem officiis, qui consequatur eveniet hic voluptatibus, pariatur itaque placeat aliquid
-                culpa repudiandae. Exercitationem temporibus placeat itaque, obcaecati cum quasi optio, id sapiente
-                inventore dicta necessitatibus eum nihil consectetur.
-                Velit at autem temporibus. Sed voluptate laborum earum fugit ipsam eos minus labore a esse fuga placeat
-                quae tempora excepturi, hic, ex voluptatem officia dolores nobis optio. Excepturi, amet animi!
-                Dolorum hic totam, vero soluta corporis voluptatem quibusdam eaque possimus, tenetur ratione quisquam
-                cupiditate esse enim! Officia beatae minima dolores quia explicabo, culpa asperiores deleniti itaque?
-                Vitae facilis exercitationem error.</p>
-                <div class="images">
-                <img class="float-left" src="https://www.colorbook.io/imagecreator.php?hex=0000ff&width=255&height=255&text=text">
-                <img class="float-left" src="https://www.colorbook.io/imagecreator.php?hex=4fcdff&width=255&height=255&text=text">
-                <img class="float-left" src="https://www.colorbook.io/imagecreator.php?hex=4fcbaa&width=255&height=255&text=text">
-                <div class="clear-fix"></div>
-                </div>
-            <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, qui suscipit modi nemo cumque</div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem temporibus quasi enim quisquam? Illo
-                similique nihil est quis dignissimos quae ea eos pariatur. Enim fuga nobis necessitatibus temporibus,
-                dolore dolorem.
-                Quia vero eligendi, rem ipsam iste aliquid fugit expedita totam, suscipit ratione eos dolores
-                perferendis nihil, iusto commodi id distinctio perspiciatis debitis tempora! Sunt animi nisi voluptatem
-                earum laboriosam. Harum.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt totam quas inventore cupiditate! Esse
-                atque voluptas dolorum! Iste hic soluta quos pariatur sit, et voluptate impedit, exercitationem eveniet
-                ad fugit.
-                Molestias omnis iusto quos sapiente perspiciatis sequi tempore cupiditate voluptatibus vel nostrum
-                itaque aliquam, esse adipisci dicta at porro saepe. Modi rerum fugit consequuntur porro quas eaque ea
-                earum corporis!
-                Molestias harum id, incidunt modi deserunt corrupti eligendi voluptatibus enim vero ex, dolorem
-                obcaecati molestiae ratione doloribus cupiditate quis consequuntur repellendus delectus mollitia ipsum
-                nulla animi dolores fugit sint. Enim.
-                Earum facilis temporibus odit repudiandae ullam id consequuntur voluptatem deserunt, adipisci, nemo
-                architecto eum necessitatibus ratione quae. Dolores veniam, eveniet quod incidunt quia, voluptatum
-                voluptas quasi, minima ex porro corrupti?</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse facilis asperiores impedit dicta
-                distinctio delectus eius, perferendis quasi reprehenderit laboriosam inventore, aut facere sed aliquam
-                sequi amet blanditiis eligendi velit.</p>
-        </section>
+        <header class="header-menu">
+            <nav class="menu">
+               <ul class="items">
+                   <li class="items-menu">Header Menu</li>
+               </ul>
+            </nav>
+            <!-- /.menu -->
+        </header>
+        <!-- /.header-menu -->
+
+        
     </main>
+    <!-- /.main-page -->
+    
 </body>
 
 </html>
